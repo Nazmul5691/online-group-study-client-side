371e276e12827e5249aa4765616bf9c877564430
Nazmul5691 <nazmulislam5691@gmail.com>
2023-11-05 19:24:37 +0600

    added information in footer

--- src/pages/SharedPages/Footer.jsx
+++ src/pages/SharedPages/Footer.jsx
@@ -1,27 +1,21 @@
-import logo from '../../../public/logo.png'
+import logo from '../../../public/logo.png';
 
 const Footer = () => {
   return (
-    <footer className="footer p-10 bg-black text-white">
+    <footer className="footer px-10 py-20 bg-black text-white">
       <aside>
-        <div className='flex'>
-        <img className='w-[45px] h-[45px] rounded-full mr-3' src={logo} alt="" />
-        <h1 className="font-bold text-blue-500 text-3xl">Learn Hub</h1>
+        <div className="flex">
+          <img className="w-[45px] h-[45px] rounded-full mr-3" src={logo} alt="" />
+          <h1 className="font-bold text-blue-500 text-3xl mt-2">Learn Hub</h1>
         </div>
-        <p>Providing reliable services since 2005</p>
+        <p className="ml-2">Empowering Learning Together</p>
       </aside>
       <nav>
-        <header className="footer-title">Technology Products</header>
-        <a className="link link-hover">Laptops and Accessories</a>
-        <a className="link link-hover">Smartphones and Gadgets</a>
-        <a className="link link-hover">Electronics and Appliances</a>
-        <a className="link link-hover">Software and Applications</a>
-        <a className="link link-hover">Tech Support and Services</a>
-      </nav>
-      <nav>
-        <header className="footer-title">Company</header>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact us</a>
+        <header className="footer-title">Study Resources</header>
+        <a href="/assignment" className="link link-hover">Assignment</a>
+        <a href="/myAssignment" className="link link-hover">My Assignment</a>
+        <a href="/submittedAssignment" className="link link-hover">Submitted Assignment</a>
+        <a href="/assignment" className="link link-hover">Study Groups</a>
       </nav>
       <nav>
         <header className="footer-title">Legal</header>
@@ -29,6 +23,10 @@ const Footer = () => {
         <a className="link link-hover">Privacy policy</a>
         <a className="link link-hover">Cookie policy</a>
       </nav>
+
+      <div>
+        <h2>Copyright © 2023 Learn Hub. <br /> All rights reserved.</h2>
+      </div>
     </footer>
   );
 };
